374839da7fc8ce306acec7306ea07407bd1aee1d
lmtran <lmtran@bu.edu>
2025-05-01 17:04:52 -0400

    fix pylint

--- controller_line.py
+++ controller_line.py
@@ -19,9 +19,9 @@ class LineController(Node):
         self.create_subscription(PointStamped, '/image/centroid', self.centroid_callback, 1)
 
         self.lin_speed = 0.0
-        self.gains = {'proportional':0.0, 'derivative':0.0, 'integral':0.0}
+        self.gains = {'pro':0.0, 'der':0.0, 'int':0.0}
 
-        self.pid = controller.PID(self.gains['proportional'],self.gains['derivative'],self.gains['integral'])
+        self.pid = controller.PID(self.gains['pro'], self.gains['der'], self.gains['int'])
         self.msg_previous = None
 
     def centroid_callback(self, msg):
